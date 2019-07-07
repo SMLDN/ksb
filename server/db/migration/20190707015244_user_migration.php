cd93228a22af03339687f14b640fbf0dd7985f5b
SLDN <sakmyapeo@gmail.com>
2019-07-07 11:05:58 +0900

    #2 Framework] Thực thi tính năng migration DB

---
+++ server/db/migration/20190707015244_user_migration.php
@@ -0,0 +1,43 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class UserMigration extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    addCustomColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Any other destructive changes will result in an error when trying to
+     * rollback the migration.
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table("user", ["id" => "user_id"]);
+        $table->addColumn("login_name","string", ["limit" => 127])
+            ->addColumn("email","string", ["limit" => 255])
+            ->addColumn("created_at","timestamp", ["default" => "CURRENT_TIMESTAMP", "timezone" => false])
+            ->addColumn("updated_at","timestamp", ["timezone" => false])
+            ->addIndex(["user_id"])
+            ->addIndex(["login_name"],  ["unique"=> true, "name" =>"idx_login_name"])
+            ->create();
+    }
+}
