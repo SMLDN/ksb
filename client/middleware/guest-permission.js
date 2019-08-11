2ecd3eb8549d9f7259e204cea554207854aecb17
SLDN <sakmyapeo@gmail.com>
2019-08-11 16:18:03 +0900

    Chỉnh lại cấu trúc folder

--- client/middleware/guest-permission.js
+++
@@ -1,8 +0,0 @@
-export default function({ store, redirect }) {
-    /**
-     * Kiểm tra xem người dùng có phải chưa đăng nhập hay không?
-     */
-    if (store.getters["auth/authenticated"]) {
-        return redirect("/");
-    }
-}
